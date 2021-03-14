0e803cc34c149a58519df0c80161ac5b28f2b755
murasaki8tanimura <murasaki.tanimura@gmail.com>
2021-03-14 10:12:31 +0200

    Added laptop branch

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -1,11 +1,11 @@
 /* See LICENSE file for copyright and license details. */ /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 3;        /* Fucking Gaps */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fantasque Sans Mono:size=15" };
-static const char dmenufont[]       = "Fantasque Sans Mono:size=15";
+static const char *fonts[]          = { "Fira Code:size=15" };
+static const char dmenufont[]       = "Fira Code:size=15";
 static const char col_bg[]          = "#282a36";
 static const char col_fg[]          = "#f8f8f2";
 static const char col_purple[]      = "#bd93f9";
@@ -17,7 +17,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 
-static const char *tags[] = { "", "", "", "","","", "" };
+static const char *tags[] = { "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -25,13 +25,26 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask   switchtotag    isfloating   monitor */
-	{ "Steam",      NULL,       NULL,       1 << 6,      0,            0,           -1 },
-	{ "Audacious",  NULL,       NULL,       1 << 4,      1,       	   0,           -1 },
-	{ "Pcmanfm",    NULL,       NULL,       1 << 3,      1,            0,           -1 },
-	{ "Firefox",    NULL,       NULL,       1 << 2,      1,            0,           -1 },
-	{ "st",         NULL,       NULL,       1 << 1,      1,            0,           -1 },
-	{ "discord",    NULL,       NULL,       1 << 5,      0,            0,           -1 },
 
+	/*WORKSPACE TAP 6*/
+	{ "st",         NULL,       NULL,       1 << 1,      1,            0,           -1 },
+	{ "code",       NULL,       NULL,       1 << 1,      1,            0,           -1 },
+	{ "vim",        NULL,       NULL,       1 << 1,      1,            0,           -1 },
+
+	/*BROWSERS TAP 5*/
+	{ "firefox",  	NULL,       NULL,       1 << 2,      1,            0,           -1 },
+	{ "Chromium",  	NULL,       NULL,       1 << 2,      1,            0,           -1 },
+
+	/*FileManager TAP 4*/
+	{ "Pcmanfm",    NULL,       NULL,       1 << 3,      1,            0,           -1 },
+
+	/*FILES/MEDIA TAP 3*/
+	{ "Zathura",    NULL,       NULL,       1 << 4,      1,       	   0,           -1 },
+	{ "Sxiv",       NULL,       NULL,       1 << 4,      1,       	   0,           -1 },
+	{ "mpv",        NULL,       NULL,       1 << 4,      1,       	   0,           -1 },
+
+	/*MUSIC TAP 2*/
+	{ "Audacious",  NULL,       NULL,       1 << 5,      1,       	   0,           -1 },
 };
 
 /* layout(s) */
@@ -62,10 +75,12 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_purple, "-sf", col_bg, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *firefox[]  = { "firefox-esr", NULL };
-static const char *fm[]  = { "pcmanfm", NULL };
+static const char *dmenucmd[] 	= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_purple, "-sf", col_bg, NULL };
+static const char *termcmd[]  	= { "st", NULL };
+static const char *vim[]  	= { "st", "-e", "vim", NULL };
+static const char *firefox[]  	= { "firefox", NULL };
+static const char *chromium[]  	= { "chromium", NULL };
+static const char *fm[]  	= { "pcmanfm", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -74,6 +89,8 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,           XK_j,      pushdown,       {0} },
 	{ MODKEY|ControlMask,           XK_k,      pushup,         {0} },
 	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = firefox } },
+	{ MODKEY,		        XK_w,      spawn,          {.v = chromium } },
+	{ MODKEY|ShiftMask,             XK_v,      spawn,          {.v = vim } },
 	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = fm} },
 	{ MODKEY|ShiftMask,             XK_q,	   quit,           {0}  },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -85,7 +102,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,          	XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -99,15 +116,15 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_9,                      0)
+	TAGKEYS(                        XK_8,                      1)
+	TAGKEYS(                        XK_7,                      2)
+	TAGKEYS(                        XK_6,                      3)
 	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_4,                      5)
+	TAGKEYS(                        XK_3,                      6)
+	TAGKEYS(                        XK_2,                      7)
+	TAGKEYS(                        XK_1,                      8)
 };
 
 /* button definitions */
--- st-0.8.4/config.h
+++ st-0.8.4/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fantasque Sans Mono:pixelsize=17:antialias=true:autohint=true";
+static char *font = "Fira Code:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
