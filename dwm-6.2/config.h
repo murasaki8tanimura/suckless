8bc7d5e30f23be80ec2460b252765912a3085ae4
murasaki8tanimura <murasaki.tanimura@gmail.com>
2021-04-04 15:39:47 +0300

    FUCK

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */ /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 3;        /* Fucking Gaps */
+static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 15};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -24,6 +24,7 @@ static const Rule rules[] = {
 
 	/* WORKSPACE */
 	{ "st",         NULL,       NULL,       1 << 1,      1,            0,           -1 },
+	{ "URxvt",      NULL,       NULL,       1 << 1,      1,            0,           -1 },
 	{ "code-oss",   NULL,       NULL,       1 << 1,      1,            0,           -1 },
 
 	/* INTERNET */
@@ -76,6 +77,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_purple, "-sf", col_bg, NULL };
 static const char *termcmd[]  		= { "st", NULL };
+static const char *secondterm[]	 	= { "urxvt", NULL };
 static const char *firefox[]  		= { "firefox", NULL };
 static const char *chromium[]  		= { "chromium", NULL };
 static const char *filemanager[]	= { "pcmanfm", NULL };
@@ -86,6 +88,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,           XK_Return, spawn,          {.v = secondterm} },
 	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = firefox} },
 	{ MODKEY,                       XK_w,      spawn,          {.v = chromium } },
 	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = filemanager} },
@@ -94,6 +97,10 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,           XK_j,      pushdown,       {0} },
 	{ MODKEY|ControlMask,           XK_k,      pushup,         {0} },
 	{ MODKEY|ControlMask,           XK_q,	   quit,           {0}  },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -116,15 +123,15 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_9,                      0)
-	TAGKEYS(                        XK_8,                      1)
-	TAGKEYS(                        XK_7,                      2)
-	TAGKEYS(                        XK_6,                      3)
+	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_6,                      5)
 	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_4,                      5)
-	TAGKEYS(                        XK_3,                      6)
-	TAGKEYS(                        XK_2,                      7)
-	TAGKEYS(                        XK_1,                      8)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_1,                      0)
 };
 
 /* button definitions */
